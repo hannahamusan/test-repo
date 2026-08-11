9be849583a248ec367e1c691105a49989cfbde42
Amusan Hannah <amusanhannah21@gmail.com>
2026-08-11 09:40:01 +0100

    new file added

---
+++ custom_module.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    """This function adds two numbers.
+    Args:
+        It accepts two arguments: x&Y
+        """
+    return x + y
+
+
+
+
+def celsius_to_fahrenheit(celsius):
+    
+    return(celsius* 9/5) + 32
+
+
+
+name = 'Hannah'
